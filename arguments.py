d6191b04125aac11f0cd1305ab534dca6a0c62cd
Josh Trommel <josh@imjosh.me>
2014-04-07 15:53:51 -0700

    More files :D

--- arguments.py
+++ arguments.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-import sys
+from sys import argv
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+# print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
